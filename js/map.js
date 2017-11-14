bc655b881067c763224cd19f900683ec85e4e99d
sammraby@gmail.com <Sam@Sams-MacBook-Pro-4.local>
2017-11-14 16:28:00 -0500

    added legend toggle

--- js/map.js
+++ js/map.js
@@ -199,4 +199,21 @@ var div = L.DomUtil.create('div', 'info legend'),
     return div;
 };
 
+showLegend = true;  // default value showing the legend
+
 legend.addTo(map);
+
+//LEGEND TOGGLE 
+
+var toggleLegend = function(){
+        if(showLegend === true){
+        /* use jquery to select your DOM elements that has the class 'legend' */
+           $('.legend').hide(); 
+           showLegend = false; 
+        }else{
+           $('.legend').show();
+           showLegend = true; 
+        }
+    }
+
+
